11d1fa0b868a625a7752922ef2404d7b177c8516
matt <ml16847@bristol.ac.uk>
2021-11-10 17:26:04 +0000

    upload stuff for index compile

---
+++ index/data/index/colour_palette.R
@@ -0,0 +1,10 @@
+##### Packages
+library(wesanderson)
+
+##### Palettes
+d1 <- wes_palette("Royal1", type = "discrete")
+d2 <- wes_palette("GrandBudapest2", type = "discrete")
+d3 <- wes_palette("Cavalcanti1", type = "discrete")
+d4 <- wes_palette("Darjeeling1", type = "discrete")
+discrete_wes_pal <- c(d1, d2, d3, d4)
+continuous_wes_pal <- wes_palette("Zissou1", 100, type = "continuous")
